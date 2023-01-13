ef372e64c308d070fbae09945370d83a98f9f579
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-01-14 00:11:10 +0100

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication yes',
-  replace => true
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
+file_line { 'Delare identity file':
   ensure => present,
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
-  replace => true
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
